b6a9912118c41fb6790b0cef38d1c8dbbeee9ffc
Yuda Kusuma <yuda.kusuma@student.umn.ac.id>
2025-09-26 14:53:33 +0700

    Commit No. 1 - Retrofit Setup and Fetch API JSON Response

---
+++ app/src/main/java/com/example/lab_week_05/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.lab_week_05
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+class MainActivity : AppCompatActivity() {
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.thecatapi.com/v1/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+    }
+
+    private val catApiService by lazy {
+        retrofit.create(CatApiService::class.java)
+    }
+
+    private val apiResponseView: TextView by lazy {
+        findViewById(R.id.api_response)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Edge-to-edge handling
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        getCatImageResponse()
+    }
+
+    private fun getCatImageResponse() {
+        val call = catApiService.searchImages(1, "full")
+        call.enqueue(object : Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(MAIN_ACTIVITY, "Failed to get response", t)
+                apiResponseView.text = "Error: ${t.message}"
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful) {
+                    apiResponseView.text = response.body()
+                } else {
+                    val errorMessage = "Failed to get response\n${response.errorBody()?.string().orEmpty()}"
+                    Log.e(MAIN_ACTIVITY, errorMessage)
+                    apiResponseView.text = "Error: ${response.code()}"
+                }
+            }
+        })
+    }
+
+    companion object {
+        const val MAIN_ACTIVITY = "MAIN_ACTIVITY"
+    }
+}
+
+interface CatApiService {
+    @GET("images/search")
+    fun searchImages(
+        @Query("limit") limit: Int,
+        @Query("size") size: String
+    ): Call<String>
+}
